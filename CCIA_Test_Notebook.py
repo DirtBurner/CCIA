2e2aa86a54aef8070d509d6912f96db64e315f16
dirtburner <berosenheim@gmail.com>
2023-03-01 15:17:47 -0500

    Add plotting function and start analysis

--- CCIA_Test_Notebook.py
+++ CCIA_Test_Notebook.py
@@ -21,8 +21,8 @@ import DB_LGR_py as dblgr
 
 # %%
 #To import data files, you need to enter their names as strings here:
-DB_file = 'DB-2274-20230210.txt'
-ccia_file = 'ccia_10Feb2023_f0000.txt'
+DB_file = 'DB-2275-20230216.txt'
+ccia_file = 'ccia_16Feb2023_f0000.txt'
 
 
 # %% [markdown]
@@ -34,14 +34,16 @@ ccia_file = 'ccia_10Feb2023_f0000.txt'
 # %%
 db_df = dblgr.import_DB(DB_file)
 ccia_df = dblgr.import_LGR(ccia_file)
-
+print(db_df.head())
+print(type(db_df['Time'][0]))
 #Mask the run for the only the data from the experimental time (between begin and end)
 #We may be able to fold this into a function as well. 
-begin = pd.to_datetime('02/9/23 9:00:00')
-end = pd.to_datetime('02/9/23 12:00:00')
+begin = pd.to_datetime('2023-02-16 11:00:00')
+end = pd.to_datetime('2023-02-16 13:00:00')
 ccia_run_df = ccia_df.loc[(ccia_df['Time']>begin) & (ccia_df['Time']<end)]
 db_run_df = db_df.loc[(db_df['Time']>begin) & (db_df['Time']<end)]
 print(db_run_df.head())
+print(ccia_df.shape)
 print(ccia_run_df.columns[0:15]) #There are 105 columns of data in ccia_df. It is useful to know the first few.
 
 # %% [markdown]
@@ -52,10 +54,77 @@ print(ccia_run_df.columns[0:15]) #There are 105 columns of data in ccia_df. It i
 #Make a plot, add time offset with pd.Timedelta function
 #(This cell will be eventually made into one line of code when we decide what we want to plot.)
 
-del_time = pd.Timedelta(hours=24)
-fig1, ax1 = plt.subplots(nrows=3, ncols=1)
+del_time = pd.Timedelta(hours=0) #This accounts for any difference in times between the computers.
+fig1, ax1 = plt.subplots(nrows=3, ncols=1, sharex=True)
 ax1[0].plot(ccia_run_df['Time'] - del_time, ccia_run_df['[CO2]_ppm'])
 ax1[1].plot(db_run_df['Time'], db_run_df['pCO2'], color='peru')
 ax1[2].plot(ccia_run_df['Time'] - del_time, ccia_run_df['[CO2]_ppm'])
 ax1[2].plot(db_run_df['Time'], db_run_df['pCO2'], color='peru')
 
+
+# %%
+print(ccia_run_df.head())
+
+
+# %%
+def import_DB(file):
+    db_df = pd.read_csv(
+        file,
+        delimiter='\t',
+        header=None, 
+        names=[
+            'Time',
+            'pCO2',
+            'Temperature',
+            'He_main',
+            'He_side',
+            'O2'
+        ]
+    )
+    print('Time elapsed = ', (max(db_df['Time']) - min(db_df['Time']))/60, ' minutes')
+    db_df = convert_dates(db_df)
+
+    return db_df
+
+def convert_dates(df):
+    '''Convert the dates in date_tiem column from LabView to EST.
+    '''
+    #debug('Operating in debug mode.', '\n')
+   # set time zone relative to GMT
+    dh = -5  # EST
+    dy = -66  # to deal with LabView's 65 year and 40 week future offset
+
+    # make 'date_time' column into timestamp
+    #- pd.Timedelta(weeks=dy*52-12, hours=dh)
+    df['Time'] = [pd.to_datetime(time, unit='s', origin='unix') for time in db_df['Time']] 
+    print(df['Time'][0:5])
+    a = [timestamp.strftime('%Y-%m-%d %H:%M:%S') for timestamp in df['Time']]
+    print(a[0:5])
+    # now get it into a meaningful time
+    df["Time"] = pd.to_datetime(a) 
+    
+    return df
+
+test_df = import_DB(DB_file)
+print(test_df.head())
+print(type(test_df['Time'][0]))
+
+# %%
+t_df = pd.read_csv(
+        DB_file,
+        delimiter='\t',
+        header=None, 
+        names=[
+            'Time',
+            'pCO2',
+            'Temperature',
+            'He_main',
+            'He_side',
+            'O2'
+        ]
+    )
+
+print('Time elapsed = ', (max(t_df['Time']) - min(t_df['Time']))/60, ' minutes')
+print(type(pd.to_datetime(t_df['Time'][0])))
+t_df['Time'] = pd.to_datetime(t_df['Time'], origin='unix', unit='s') + pd.Timedelta(weeks=-66*52-12+1/7, hours=-6)
+print(t_df.head())
--- DB_LGR_py.py
+++ DB_LGR_py.py
@@ -46,6 +46,8 @@ def import_LGR(file):
     col_names = [a.lstrip() for a in ccia_df.columns]
     ccia_df.columns = col_names
     ccia_df['Time'] = pd.to_datetime(ccia_df['Time'])
+    a = [timestamp.strftime('%Y-%m-%d %H:%M:%S') for timestamp in ccia_df['Time']]
+    ccia_df['Time'] = pd.to_datetime(a)
 
 
     return ccia_df
@@ -59,9 +61,64 @@ def convert_dates(df):
     dy = -66  # to deal with LabView's 65 year and 40 week future offset
 
     # make 'date_time' column into timestamp
-    df["Time"] = pd.to_datetime(df["Time"], unit="s")
-
-    # now get it into a meaningful time
-    df["Time"] = df["Time"] + pd.Timedelta(weeks=dy*52-12, hours=dh)
+    #- pd.Timedelta(weeks=dy*52-12, hours=dh)
+    df['Time'] = pd.to_datetime(
+        df['Time'],
+        origin='unix',
+        unit='s'
+    )  + pd.Timedelta(weeks=dy*52-12+1/7, hours=dh)
+    
+   
 
     return df
+
+def view_concentrations(ccia_df, db_df, begin_timestamp, end_timestamp, time_offset):
+    '''
+    Initial data view comparison of the [CO2] data in the dirtburner and in the LRG CCIA.
+    This function takes the two dataframes, two timestamps marking the beginning and end of 
+    the data gathering period, and plots the data in a 3-panel plot. The time offset is for 
+    adjusting the time if the computers are registering different times for the duration of the runs
+
+        Inputs:
+            ccia_df (dataframe) - the output of the import_LGR function. A dataframe with 105 columns rows 
+                from approximately every second of data acquisition.
+            db_df (dataframe) - the output of the import_DB function. A dataframe with 7 columns and rows
+                from approxaimtely every second of data acquisition.
+            begin_timestamp (pandas timestamp) - a string in the format 'YYYY-mm-dd HH:MM' to mark the beginning
+                of the experiment
+            end_timestamp (pandas timestamp) - a string in the format 'YYYY-mm-dd HH:MM' to mark the end
+                of the experiment
+            time_offset (floating point number) - The number of hours of offset there is between the CCIA and 
+                DB computer, if there is any offset. Good laboratory practice would be to ensure that there is 
+                no offset, however this allows you to adjust for instance, if you have to reboot the linux system
+                controlling the LGR CCIA system.
+        
+        Outputs:
+            ax1 (matplotlib axes handle) - this is the handle returned so that you can make changes to the axes
+                (axis labels, fonts, etc.) after using the function. There are three axes, 0 is the top and 2 
+                is the bottom
+
+
+    '''
+    #Mask the dataframes to isolate only the data that the viewer wants to view. 
+    begin = pd.to_datetime(begin_timestamp)
+    end = pd.to_datetime(end_timestamp)
+    ccia_run_df = ccia_df.loc[(ccia_df['Time']>begin) & (ccia_df['Time']<end)]
+    db_run_df = db_df.loc[(db_df['Time']>begin) & (db_df['Time']<end)]
+    
+    del_time = pd.Timedelta(hours=time_offset) #This accounts for any difference in times between the computers.
+    fig1, ax1 = plt.subplots(nrows=3, ncols=1, sharex=True)
+    ax1[0].plot(ccia_run_df['Time'] - del_time, ccia_run_df['[CO2]_ppm'])
+    ax1[0].set(title='LGR')
+    ax1[1].plot(db_run_df['Time'], db_run_df['pCO2'], color='peru')
+    ax1[1].set(title='DB')
+    plt.xticks(rotation=30, ha='right')
+    ax1[2].plot(ccia_run_df['Time'] - del_time, ccia_run_df['[CO2]_ppm'])
+    ax1[2].plot(db_run_df['Time'], db_run_df['pCO2'], color='peru')
+
+    ax1[2].set(xlabel='Time', title='DB and LGR')
+    big_ax1 = fig1.add_subplot(111, frameon=False)
+    plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
+    big_ax1.set(ylabel=r'pCO$_{2}$, ppm')
+
+    return ax1
